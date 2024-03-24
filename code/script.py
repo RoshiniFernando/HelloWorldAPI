136bc5f2f9a266c955abe01f5f576771a308b2fd
RoshiniFernando <iresharoshini7@gmail.com>
2024-03-24 19:37:29 +0000

    update with .yaml files

--- code/script.py
+++ code/script.py
@@ -1,1 +1,10 @@
-print('Hello World!')
+import json
+import ast
+from flask import Flask, request
+from types import SimpleNamespace
+app = Flask(__name__)
+
+# route relevant to the hello world
+@app.route('/hw/healthCheck', methods=['GET'])
+def health_check():
+    return "Hello, World!!!"
